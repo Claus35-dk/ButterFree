3866e6637751fb3d3967462410e68fe01d64be0e
Lasse Vork Borchert <monex_9@msn.com>
2012-09-13 12:27:26 +0200

    Add AS37B

---
+++ AS37/CollectionPerformanceTest/CollectionPerformanceTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CollectionPerformanceTest
+{
+    class Program
+    {
+        static int TestScale = 1000000;
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
