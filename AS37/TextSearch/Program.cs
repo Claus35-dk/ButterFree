9bc0bd081d720e971dec148e1f87ceec387dd3fa
Michael Soeby Andersen <michael@mileratec.com>
2012-09-13 21:07:08 +0200

    Fixed date regex and added the custom input regex

--- AS37/TextSearch/Program.cs
+++ AS37/TextSearch/Program.cs
@@ -16,11 +16,21 @@ namespace TextSearch {
     private TextSearch(string input, string[] userPatterns) {
       foreach (string pattern in userPatterns) {
         string groupName = "G" + regexCounter++;
-        patterns.Add(groupName, new RegExPattern(groupName, pattern));
+        patterns.Add(groupName, new RegExPattern(groupName, convertToRegEx(pattern)));
       }
+      string days = @"(mon|tue|wed|thu|fri|sat|sun)";
+      string months = @"(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)";
       patterns.Add("URL", new RegExPattern("URL", @"\b(?:\S+)://(\S+)\b", ConsoleColor.Blue, ConsoleColor.White));
-      patterns.Add("Date", new RegExPattern("Date", @"\b((0?[0-9])|(1[0 -2])):[0-5][0-9]\b", ConsoleColor.Red, ConsoleColor.White));
+      patterns.Add("Date", new RegExPattern("Date", days + @"(,)?\s" + "(([0-2][0-9])|(30))" + @"\s" + months + @"\s" + @"[0-9]{4}\s((0?[0-9])|(([0-1][0-9])|(2[0-3]))):[0-5][0-9]:[0-5][0-9]\s[\+|-][0-9]{4}", ConsoleColor.Red, ConsoleColor.White));
       highlightRegExMatches(input);
+    }
+
+    private string convertToRegEx(string input) {
+      input = input.Replace("+", @"\W?");
+      if (input.Substring(0, 1).Equals("*")) input = input.Replace("*", @"\w*?");
+      else if (input.Substring(input.Length - 1, 1).Equals("*")) input = input.Replace("*", @"\w*?");
+      else input = input.Replace("*", @"\w*");
+      return @"\b" + input + @"\b";
     }
 
     private void highlightRegExMatches(string haystack) {
@@ -29,7 +39,7 @@ namespace TextSearch {
         entirePattern.Append(pattern.RegEx + "|");
       }
       string needle = entirePattern.ToString().Substring(0, entirePattern.Length - 1);
-      Regex regex = new Regex(needle);
+      Regex regex = new Regex(needle, RegexOptions.IgnoreCase);
       MatchCollection matches = regex.Matches(haystack);
       int stringIndex = 0;
       foreach (Match match in matches) {
@@ -57,18 +67,20 @@ namespace TextSearch {
       public ConsoleColor BgColor { set; get; }
       public ConsoleColor FgColor { set; get; }
 
-      public RegExPattern(string GroupName, string RegEx, ConsoleColor BgColor, ConsoleColor FgColor) : this() {
+      public RegExPattern(string GroupName, string RegEx, ConsoleColor BgColor, ConsoleColor FgColor)
+        : this() {
         this.GroupName = GroupName;
         this.RegEx = "(?<" + GroupName + ">" + RegEx + ")";
         this.BgColor = BgColor;
         this.FgColor = FgColor;
       }
 
-      public RegExPattern(string GroupName, string RegEx) : this() {
+      public RegExPattern(string GroupName, string RegEx)
+        : this() {
         this.GroupName = GroupName;
         this.RegEx = "(?<" + GroupName + ">" + RegEx + ")";
         this.BgColor = ConsoleColor.Yellow;
-        this.FgColor = ConsoleColor.White;
+        this.FgColor = ConsoleColor.Black;
       }
     }
   }
